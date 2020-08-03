81ca25c7967ff628f289eb98b2838ee6da8f317c
LCLP <lukas@lclpnet.work>
2020-08-03 16:43:46 +0200

    Snow melting now also considered as BlockStateToStateEvent

---
+++ CoreBase/src/main/java/work/lclpnet/corebase/asm/mixin/common/MixinSnowBlock.java
@@ -0,0 +1,35 @@
+package work.lclpnet.corebase.asm.mixin.common;
+
+import java.util.Random;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SnowBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.MinecraftForge;
+import work.lclpnet.corebase.event.custom.BlockStateToStateEvent;
+
+@Mixin(SnowBlock.class)
+public class MixinSnowBlock {
+
+	@Inject(
+			method = "Lnet/minecraft/block/SnowBlock;tick(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/server/ServerWorld;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "spawnDrops(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"
+					),
+			cancellable = true
+			)
+	public void beforeSpawnDrops(BlockState state, ServerWorld world, BlockPos pos, Random ran, CallbackInfo ci) {
+		BlockStateToStateEvent event = new BlockStateToStateEvent(world, pos, state, Blocks.AIR.getDefaultState());
+		MinecraftForge.EVENT_BUS.post(event);
+		if(event.isCanceled()) ci.cancel();
+	}
+
+}
--- CoreBase/src/main/java/work/lclpnet/corebase/event/EventListener.java
+++ CoreBase/src/main/java/work/lclpnet/corebase/event/EventListener.java
@@ -7,4 +7,10 @@ import work.lclpnet.corebase.CoreBase;
 @EventBusSubscriber(modid = CoreBase.MODID, bus = Bus.FORGE)
 public class EventListener {
 	
+	/*@SubscribeEvent
+	public static void onSnowMelt(BlockStateToStateEvent e) {
+		e.setCanceled(true);
+		System.out.println("STS: " + e.getState() + " -> " + e.getTo());
+	}*/
+	
 }
