0f37a27d4abcade1b3c177d13846e4a5528ab44b
LCLP <lukas@lclpnet.work>
2020-10-07 10:34:20 +0200

    CommandPlayerInfo fixed on client + improve attributes

--- CoreBase/src/main/java/work/lclpnet/corebase/cmd/CommandPlayerInfo.java
+++ CoreBase/src/main/java/work/lclpnet/corebase/cmd/CommandPlayerInfo.java
@@ -1,6 +1,9 @@
 package work.lclpnet.corebase.cmd;
 
+import java.lang.reflect.Field;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
@@ -8,17 +11,18 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
+import io.netty.channel.local.LocalAddress;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.command.arguments.EntitySelector;
 import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifierManager;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.*;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import work.lclpnet.corebase.CoreBase;
 import work.lclpnet.corebase.util.MessageType;
 import work.lclpnet.corebase.util.ObjectHelper;
@@ -61,57 +65,64 @@ public class CommandPlayerInfo extends CommandBase{
 	}
 
 	private void info(CommandSource source, ServerPlayerEntity target) {
-		msg(source, "GENRAL", null);
-		msg(source, "NAME", target.getName().getString());
-		msg(source, "UUID", target.getUniqueID().toString());
-		msg(source, "IP-ADRESS", ((InetSocketAddress) target.connection.getNetworkManager().getRemoteAddress()).getHostString());
-		msg(source, "LANGUAGE", (String) ObjectHelper.get(target, "language"));
+		try {
+			msg(source, "GENERAL", null);
+			msg(source, "NAME", target.getName().getString());
+			msg(source, "UUID", target.getUniqueID().toString());
 
-		msg(source, "STATUS", null);
-		msg(source, "GAMEMODE", target.interactionManager.getGameType().name());
-		msg(source, "HEALTH", String.format("%.2f", target.getHealth()));
-		msg(source, "FOOD-LEVEL", target.getFoodStats().getFoodLevel());
-		msg(source, "SATURAION-LEVEL", String.format("%.2f", target.getFoodStats().getSaturationLevel()));
-		msg(source, "ALLOW-FLIGHT", target.abilities.allowFlying);
-		msg(source, "EXPERIENCE", target.experience);
-		msg(source, "EXP-LEVEL", target.experienceLevel);
-		msg(source, "TOTAL-EXP", target.experienceTotal);
-		msg(source, "WALKSPEED", target.abilities.getWalkSpeed());
-		msg(source, "FLYSPEED", target.abilities.getFlySpeed());
-		msg(source, "DIMENSION", target.world.func_234923_W_());
-		msg(source, "INVULNERABLE", target.abilities.disableDamage);
+			if(target.connection.getNetworkManager().isLocalChannel()) msg(source, "IP-ADDRESS", target.connection.getNetworkManager().getRemoteAddress().toString());
+			else msg(source, "IP-ADDRESS", ((InetSocketAddress) target.connection.getNetworkManager().getRemoteAddress()).getHostString());
 
-		msg(source, "ATTRIBUTES", null); // TODO get from attributes modifier map of entity
-		attr(source, "ATTACK-SPEED", target, Attributes.field_233825_h_);
-		attr(source, "ARMOR", target, Attributes.field_233826_i_);
-		attr(source, "ARMOR-TOUGHNESS", target, Attributes.field_233827_j_);
-		attr(source, "ATTACK-DAMAGE", target, Attributes.field_233823_f_);
-		attr(source, "KNOCKBACK-RESISTANCE", target, Attributes.field_233820_c_);
-		attr(source, "LUCK", target, Attributes.field_233828_k_);
-		attr(source, "MAX-HEALTH", target, Attributes.field_233818_a_);
-		attr(source, "MOVEMENT-SPEED", target, Attributes.field_233821_d_);
-		attr(source, "FLYING-SPEED", target, Attributes.field_233822_e_);
+			msg(source, "LANGUAGE", (String) ObjectHelper.get(target, "language"));
+
+			msg(source, "STATUS", null);
+			msg(source, "GAME-MODE", target.interactionManager.getGameType().name());
+			msg(source, "HEALTH", String.format("%.2f", target.getHealth()));
+			msg(source, "FOOD-LEVEL", target.getFoodStats().getFoodLevel());
+			msg(source, "SATURATION-LEVEL", String.format("%.2f", target.getFoodStats().getSaturationLevel()));
+			msg(source, "ALLOW-FLIGHT", target.abilities.allowFlying);
+			msg(source, "EXPERIENCE", target.experience);
+			msg(source, "EXP-LEVEL", target.experienceLevel);
+			msg(source, "TOTAL-EXP", target.experienceTotal);
+			msg(source, "WALK-SPEED", target.abilities.getWalkSpeed());
+			msg(source, "FLY-SPEED", target.abilities.getFlySpeed());
+			msg(source, "DIMENSION", target.world.func_234923_W_().func_240901_a_());
+			msg(source, "INVULNERABLE", target.abilities.disableDamage);
+
+			msg(source, "ATTRIBUTES", null); // TODO get from attributes modifier map of entity
+			try {
+				Field f = ObfuscationReflectionHelper.findField(AttributeModifierManager.class, "field_233775_b_");
+				f.setAccessible(true);
+				@SuppressWarnings("unchecked")
+				Map<Attribute, ModifiableAttributeInstance> map = (Map<Attribute, ModifiableAttributeInstance>) f.get(target.func_233645_dx_());
+				System.out.println(map);
+				map.forEach((attr, instance) -> msg(source, new TranslationTextComponent(attr.func_233754_c_()), instance.getValue()));
+			} catch (ReflectiveOperationException e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void attr(CommandSource source, String category, ServerPlayerEntity target, Attribute attr) {
-		ModifiableAttributeInstance attribute = target.getAttribute(attr);
-		if(attribute != null) msg(source, category, attribute.getValue());
-	}
-
-	private void msg(CommandSource source, String category, @Nullable Object value) {
-		if(value == null) source.sendFeedback(TextComponentHelper.applyTextStyle(new StringTextComponent(category), TextFormatting.BLUE), false);
+	private void msg(CommandSource source, IFormattableTextComponent itc, @Nullable Object value) {
+		if(value == null) source.sendFeedback(TextComponentHelper.applyTextStyle(itc, TextFormatting.BLUE), false);
 		else {
 			ITextComponent sibling = TextComponentHelper.applyTextStyle(new StringTextComponent(value.toString()), TextFormatting.GREEN);
 			ITextComponent feedback = TextComponentHelper.appendSibling(
 					TextComponentHelper.appendText(
 							TextComponentHelper.applyTextStyle(
-									new StringTextComponent(category), 
+									itc,
 									TextFormatting.DARK_PURPLE),
-							": "), 
+							": "),
 					sibling);
-			
+
 			source.sendFeedback(feedback, false);
 		}
+	}
+
+	private void msg(CommandSource source, String category, @Nullable Object value) {
+		msg(source, new StringTextComponent(category), value);
 	}
 
 }
